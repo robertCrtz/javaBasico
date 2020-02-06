3c4d304c64ee9ab12398c24e439f144fa4aa7cbf
Roberto <robertoenrique98@hotmail.com>
2020-02-06 14:51:35 -0600

    Codigo fuente de boolean y char listos para el testing

---
+++ Caracteres.java
@@ -0,0 +1,16 @@
+class Caracteres {
+ 
+    public static void main( String args[] ) {
+ 
+        char letra1, letra2;
+ 
+        letra1 = 'a';
+        letra2 = 'b';
+ 
+        System.out.print("La primera letra es : ");
+        System.out.println(letra1);
+ 
+        System.out.print("La segunda letra es : ");
+        System.out.println(letra2);
+    }
+}
---
+++ booleano.java
@@ -0,0 +1,15 @@
+public class booleano{
+	public static void main(String[] args){
+
+		boolean x;
+		x = false;
+		System.out.println("La variable x es: "+ x);
+
+		x=true;
+		System.out.println("La variable x es: "+ x);
+
+		if(x=true){
+			System.out.println("La variable x existe");
+		}
+	}
+}
